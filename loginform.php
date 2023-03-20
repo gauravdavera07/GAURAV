7ae05b0a349cc0dacabde5ed108067aa9d3cb973
gauravdavera07 <gauravdavera07@gmail.com>
2023-03-20 15:12:31 +0530

    Create loginform.php
    loginform

---
+++ loginform.php
@@ -0,0 +1,19 @@
+<html>
+	<body>
+	
+<form action="loginfgorm.php" method="post">
+  <div class="imgcontainer">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+  </div>
+</form>
+</body>
+<html>
